9fa0ae98a678fb1157e4be8d5e5ef2dea631e497
Shihab Tawsif Shown <sadadrana@gmail.com>
2024-09-17 21:27:40 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+import daisyui from 'daisyui';
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}', '*.{html,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ['light'],
+  },
+}
